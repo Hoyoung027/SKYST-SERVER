5cac7e71c956bf615c2da5ac8739ff0f343f31ef
Hoyoung027 <hybyun0207@gmail.com>
2025-05-11 03:31:55 +0900

    [feat] chatbot용 controller 생성

---
+++ src/main/java/skyst/dopamine/domain/chatbot/api/controller/ChatbotController.java
@@ -0,0 +1,34 @@
+package skyst.dopamine.domain.chatbot.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.RestController;
+import skyst.dopamine.domain.chatbot.api.dto.ChatReq;
+import skyst.dopamine.domain.chatbot.api.service.ChatbotService;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatbotController {
+
+    private final ChatbotService chatbotService;
+
+    @PostMapping("/chat")
+    public Mono<ResponseEntity<String>> chat(@RequestBody ChatReq userQuestion) {
+
+        String prompt = chatbotService.askWithHistory(userQuestion);
+
+
+
+
+        return chatbotService.chatCompletion(prompt)
+                .map(ResponseEntity::ok)
+                .onErrorResume(e -> Mono.just(
+                        ResponseEntity.status(500).body(e.getMessage())
+                ));
+    }
+
+}
